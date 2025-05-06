f39a37a3255dc2aa4dd71d185845a295ac093f1c
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-05-06 19:49:02 +0200

    hero

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,3 +7,14 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
+
+
+/*
+fontSize: {
+        'course-details-heading-small': ['26px', '36px'],
+        'course-details-heading-large': ['36px', '44px'],
+        'home-heading-small': ['28px', '34px'],
+        'home-heading-large': ['48px', '56px'],
+        'default': ['15px', '21px']
+      }
+*/
